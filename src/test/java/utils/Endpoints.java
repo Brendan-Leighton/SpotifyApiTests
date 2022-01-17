1eb2652a7c1382d13cdbb4ca873ce9e32174a045
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-17 16:54:24 -0500

    removed endpoints from inline code and put them here

---
+++ src/test/java/utils/Endpoints.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Endpoints {
+    public static final String TOKEN = "token";
+    public static final String ME = "me";
+    public static final String PLAYLISTS = "/playlists";
+    public static final String USERS = "users/";
+}
