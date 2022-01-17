3cca77bf9a2fcc22de1fd22845148f9e9c304ee4
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-17 16:18:01 -0500

    added comments to createPlaylist method

--- src/test/java/tests/Playlist.java
+++ src/test/java/tests/Playlist.java
@@ -20,24 +20,31 @@ public class Playlist {
 
     @Test
     public void createPlaylist() {
+
+        // GET USERS' PERMISSION TO ACCESS THEIR SPOTIFY INFO
+        // create requests' body
         Map<String, String> body_userToken = new HashMap<>();
         body_userToken.put("grant_type", Props.getGrantType());
         body_userToken.put("refresh_token", Props.getRefreshToken());
         body_userToken.put("client_id", Props.getClientId());
         body_userToken.put("client_secret", Props.getClientSecret());
-
+        // get response
         Response res = RestAssured.given().formParams(body_userToken).log().all()
                 .when().post(baseURI_accounts + uri_TOKEN)
                 .then().log().all().extract().response();
-
+        // extract the token that proves we have users' permission
         String access_token = res.path("access_token").toString();
 
+        // GET USERS' SPOTIFY INFO
+        // sending access_token
         Response userData = RestAssured.given().auth().oauth2(access_token).log().all()
                 .when().get(baseURI + "v1/me")
                 .then().log().all().extract().response();
-
+        // extracting users' ID
         String userId = userData.path("id");
 
+        // CREATE THE PLAYLIST
+        // uses access_token and userId from previous code
         RestAssured.given().auth().oauth2(access_token)
                 .body("{\n" +
                 "  \"name\": \"New Playlist\",\n" +
