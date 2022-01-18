b68d0bdb617bfa9c2cdbbf69bd5ea9d39cbd8d26
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-18 17:01:11 -0500

    added 'tracks'

--- src/test/java/utils/Endpoints.java
+++ src/test/java/utils/Endpoints.java
@@ -13,6 +13,7 @@ package utils;
 public class Endpoints {
     public static final String TOKEN = "token";
     public static final String ME = "me";
-    public static final String PLAYLISTS = "/playlists";
+    public static final String PLAYLISTS = "playlists";
     public static final String USERS = "users/";
+    public static final String TRACKS = "tracks";
 }
