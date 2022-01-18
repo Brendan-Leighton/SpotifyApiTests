f8e82c17d3c8ba28e55d64567f232f1a1f82ea8e
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-18 12:27:15 -0500

    add a comment for the class

--- src/test/java/utils/Endpoints.java
+++ src/test/java/utils/Endpoints.java
@@ -1,5 +1,15 @@
 package utils;
 
+/**
+ * Used with a URI, preceding the URI concatenated together in a String.
+ *
+ * <ul>
+ *      <li>TOKEN = "token";</li>
+ *      <li>ME = "me";</li>
+ *      <li>PLAYLISTS = "/playlists";</li>
+ *      <li>USERS = "users/";</li>
+ * </ul>
+ */
 public class Endpoints {
     public static final String TOKEN = "token";
     public static final String ME = "me";
