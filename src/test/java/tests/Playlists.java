53d522d58b2f5b34eb9305ab4408c01a314f98d4
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-31 11:45:48 -0500

    Update Test: 'getUsersPlaylists()' renamed 'getUsersPlaylists_hasPlaylists()'. Also, completed test.

--- src/test/java/tests/Playlists.java
+++ src/test/java/tests/Playlists.java
@@ -111,12 +111,34 @@ public class Playlists {
     //*************************
     //*************************
 
+    /*
+        TEST : GET PLAYLISTS
+        - has playlists
+        - has no playlists
+     */
+
     /**
      * GET all playlists belonging to a user
      */
-    @Test  // in progress
-    public void getUsersPlaylists() {
-        RestfulPlaylist.getAllPlaylists_forSingleUser(this.userId);
+    @Test
+    public void getUsersPlaylists_hasPlaylists() {
+        // ENSURE USER HAS 0 PLAYLISTS
+        RestfulPlaylist.deleteAllPlaylists_forSingleUser(this.userId);
+
+        // CREATE A PLAYLIST TO ADD
+        Playlist playlistToAdd = Playlist.generatePlaylist();
+
+        // ADD PLAYLIST
+        RestfulPlaylist.createPlaylist(this.userId, playlistToAdd);
+
+        // GET ALL PLAYLISTS FOR USER
+        List<Playlist> resultingPlaylists = RestfulPlaylist.getAllPlaylists_forSingleUser(this.userId);
+
+        // ASSERT
+        // user has 1 playlist
+        Assert.assertEquals(resultingPlaylists.size(), 1);
+        // playlistId matches added playlist
+        Assert.assertEquals(resultingPlaylists.get(0).getName(), playlistToAdd.getName());
     }
 
     /**
