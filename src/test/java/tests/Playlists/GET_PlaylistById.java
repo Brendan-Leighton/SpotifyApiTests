995192e581b951bb44db98e46717420510700fc4
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-31 15:00:32 -0500

    Moved Test: 'getPlaylistByPlaylistId()' to 'GET_PlaylistById.java'

---
+++ src/test/java/tests/Playlists/GET_PlaylistById.java
@@ -0,0 +1,33 @@
+package tests.Playlists;
+// TEST-NG
+import org.testng.Assert;
+import org.testng.annotations.Test;
+// MINE
+import models.Playlist;
+import utils.restResources.RestfulPlaylist;
+
+public class GET_PlaylistById extends Playlists{
+    /**
+     * GET one playlist by its ID
+     */
+    @Test
+    public void getPlaylistByPlaylistId() {
+        // CREATE PLAYLIST
+        // create request body
+        Playlist playlist = new Playlist();
+        playlist.setName("Get me by my ID! " + System.currentTimeMillis());
+        playlist.setDescription("Made to be gotten");
+        playlist.setPublic(false);
+        // send request
+        Playlist res = RestfulPlaylist.createPlaylist(userId, playlist);
+
+        // GET PLAYLIST BY ID
+        Playlist playlist_gotById = RestfulPlaylist.getPlaylist_byId(res.getId());
+
+        // ASSERT
+        Assert.assertEquals(playlist.getName(), playlist_gotById.getName());
+        Assert.assertEquals(playlist.getDescription(), playlist_gotById.getDescription());
+        Assert.assertEquals(playlist.getPublic(), playlist_gotById.getPublic());
+    }
+
+}
--- src/test/java/tests/Playlists/Playlists.java
+++ src/test/java/tests/Playlists/Playlists.java
@@ -33,29 +33,6 @@ public class Playlists {
         userId = userData.path("id");
     }
 
-    /**
-     * GET one playlist by its ID
-     */
-    @Test
-    public void getPlaylistByPlaylistId() {
-        // CREATE PLAYLIST
-        // create request body
-        Playlist playlist = new Playlist();
-        playlist.setName("Get me by my ID! " + System.currentTimeMillis());
-        playlist.setDescription("Made to be gotten");
-        playlist.setPublic(false);
-        // send request
-        Playlist res = RestfulPlaylist.createPlaylist(userId, playlist);
-
-
-        // GET PLAYLIST BY ID
-        Playlist playlist_gotById = RestfulPlaylist.getPlaylist_byId(res.getId());
-
-        // ASSERT
-        Assert.assertEquals(playlist.getName(), playlist_gotById.getName());
-        Assert.assertEquals(playlist.getDescription(), playlist_gotById.getDescription());
-        Assert.assertEquals(playlist.getPublic(), playlist_gotById.getPublic());
-    }
 
     /*
 
