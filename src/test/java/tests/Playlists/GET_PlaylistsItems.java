4442eaa604715a5da483d1358dd14c49b64ce605
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-31 15:05:14 -0500

    Moved Test: 'getPlaylistItems_NotEmpty' to 'GET_PlaylistsItems.java'

---
+++ src/test/java/tests/Playlists/GET_PlaylistsItems.java
@@ -0,0 +1,54 @@
+package tests.Playlists;
+
+import models.Playlist;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.restResources.RestfulPlaylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TEST : GET PLAYLISTS' ITEMS
+ *
+ * Test
+ *    - not empty playlist
+ *    - empty playlist
+ */
+public class GET_PlaylistsItems extends Playlists{
+
+    /**
+     * GET items from a non-empty playlist
+     */
+    @Test
+    public void getPlaylistItems_NotEmpty() {
+        // CREATE PLAYLIST
+        // create request body
+        Playlist playlist = new Playlist();
+        playlist.setName("Get me by my ID! " + System.currentTimeMillis());
+        playlist.setDescription("Made to be gotten");
+        playlist.setPublic(false);
+        // send request
+        Playlist res = RestfulPlaylist.createPlaylist(userId, playlist);
+        // get id
+        String playlistId = res.getId();
+
+        // ADD TRACK
+        List<Playlist> featuredPlaylists = RestfulPlaylist.getAllPlaylists_featured();
+        JSONObject trackThatWasAdded = RestfulPlaylist.getPlaylistsTracks(featuredPlaylists.get(0).getId()).get(0);
+        String songUri = trackThatWasAdded.get("uri").toString();
+        List<String> newItems = new ArrayList<>();
+        newItems.add(songUri);
+        RestfulPlaylist.addItemsToPlaylist(playlistId, newItems);
+
+        // TEST FUNCTION
+        List<JSONObject> tracksToCheck = RestfulPlaylist.getPlaylistsTracks(playlistId);
+
+        // ASSERT
+        // has an item
+        Assert.assertEquals(tracksToCheck.size(), 1);
+        // has correct item
+        Assert.assertEquals(tracksToCheck.get(0).get("name"), trackThatWasAdded.get("name"));
+    }
+}
--- src/test/java/tests/Playlists/Playlists.java
+++ src/test/java/tests/Playlists/Playlists.java
@@ -34,50 +34,6 @@ public class Playlists {
     }
 
 
-    /*
-
-        TEST : GET PLAYLISTS' ITEMS
-        - not empty playlist
-        - empty playlist
-
-     */
-
-    /**
-     * GET items from a playlist.
-     *
-     * "items" could be songs, podcasts, etc.
-     */
-    @Test
-    public void getPlaylistItems_NotEmpty() {
-        // CREATE PLAYLIST
-        // create request body
-        Playlist playlist = new Playlist();
-        playlist.setName("Get me by my ID! " + System.currentTimeMillis());
-        playlist.setDescription("Made to be gotten");
-        playlist.setPublic(false);
-        // send request
-        Playlist res = RestfulPlaylist.createPlaylist(userId, playlist);
-        // get id
-        String playlistId = res.getId();
-
-        // ADD TRACK
-        List<Playlist> featuredPlaylists = RestfulPlaylist.getAllPlaylists_featured();
-        JSONObject trackThatWasAdded = RestfulPlaylist.getPlaylistsTracks(featuredPlaylists.get(0).getId()).get(0);
-        String songUri = trackThatWasAdded.get("uri").toString();
-        List<String> newItems = new ArrayList<>();
-        newItems.add(songUri);
-        RestfulPlaylist.addItemsToPlaylist(playlistId, newItems);
-
-        // TEST FUNCTION
-        List<JSONObject> tracksToCheck = RestfulPlaylist.getPlaylistsTracks(playlistId);
-
-        // ASSERT
-        // has an item
-        Assert.assertEquals(tracksToCheck.size(), 1);
-        // has correct item
-        Assert.assertEquals(tracksToCheck.get(0).get("name"), trackThatWasAdded.get("name"));
-    }
-
     @Test
     public void getFeaturedPlaylists() {
         List<Playlist> playlists = RestfulPlaylist.getAllPlaylists_featured();
