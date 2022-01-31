e664ea2a49f8f51d85806e7885af18c36b8a1af3
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-01-31 14:42:49 -0500

    moved test, 'getUsersPlaylists_hasPlaylists()' to 'AddItemsToPlaylist.java'

---
+++ src/test/java/tests/Playlists/GET_UsersPlaylist.java
@@ -0,0 +1,41 @@
+package tests.Playlists;
+// JAVA
+import java.util.List;
+// TEST-NG
+import org.testng.Assert;
+import org.testng.annotations.Test;
+// MINE
+import utils.restResources.RestfulPlaylist;
+import models.Playlist;
+
+public class GET_UsersPlaylist extends Playlists{
+    /*
+        TEST : GET PLAYLISTS
+        - has playlists
+        - has no playlists
+     */
+
+    /**
+     * GET all playlists belonging to a user
+     */
+    @Test
+    public void getUsersPlaylists_hasPlaylists() {
+        // ENSURE USER HAS 0 PLAYLISTS
+        RestfulPlaylist.deleteAllPlaylists_forSingleUser(this.userId);
+
+        // CREATE A PLAYLIST TO ADD
+        Playlist playlistToAdd = Playlist.generatePlaylist();
+
+        // ADD PLAYLIST
+        RestfulPlaylist.createPlaylist(this.userId, playlistToAdd);
+
+        // GET ALL PLAYLISTS FOR USER
+        List<Playlist> resultingPlaylists = RestfulPlaylist.getAllPlaylists_forSingleUser(this.userId);
+
+        // ASSERT
+        // user has 1 playlist
+        Assert.assertEquals(resultingPlaylists.size(), 1);
+        // playlistId matches added playlist
+        Assert.assertEquals(resultingPlaylists.get(0).getName(), playlistToAdd.getName());
+    }
+}
--- src/test/java/tests/Playlists/Playlists.java
+++ src/test/java/tests/Playlists/Playlists.java
@@ -33,42 +33,6 @@ public class Playlists {
         userId = userData.path("id");
     }
 
-    //*************************
-    //*************************
-    //       GET TESTS
-    //*************************
-    //*************************
-
-    /*
-        TEST : GET PLAYLISTS
-        - has playlists
-        - has no playlists
-     */
-
-    /**
-     * GET all playlists belonging to a user
-     */
-    @Test
-    public void getUsersPlaylists_hasPlaylists() {
-        // ENSURE USER HAS 0 PLAYLISTS
-        RestfulPlaylist.deleteAllPlaylists_forSingleUser(this.userId);
-
-        // CREATE A PLAYLIST TO ADD
-        Playlist playlistToAdd = Playlist.generatePlaylist();
-
-        // ADD PLAYLIST
-        RestfulPlaylist.createPlaylist(this.userId, playlistToAdd);
-
-        // GET ALL PLAYLISTS FOR USER
-        List<Playlist> resultingPlaylists = RestfulPlaylist.getAllPlaylists_forSingleUser(this.userId);
-
-        // ASSERT
-        // user has 1 playlist
-        Assert.assertEquals(resultingPlaylists.size(), 1);
-        // playlistId matches added playlist
-        Assert.assertEquals(resultingPlaylists.get(0).getName(), playlistToAdd.getName());
-    }
-
     /**
      * GET one playlist by its ID
      */
