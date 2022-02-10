38494ed1a4b79f95b4186c3c0f4ecbe3a0709c04
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-02-10 15:18:35 -0500

    Added Comments: outlining tests to complete and endpoints' requirments.

--- src/test/java/tests/Playlists/CreatePlaylist.java
+++ src/test/java/tests/Playlists/CreatePlaylist.java
@@ -10,20 +10,31 @@ public class CreatePlaylist extends Playlists{
         TEST : CREATE PLAYLIST
 
         Requirements
-            1. userId
-            2. name - String
+            1. QUERY: userId - String
+            2. BODY: name - String
         Optional
-            1. public - boolean
-            2. collaborative - boolean
-            3. description - String
+            1. BODY: public - boolean
+            2. BODY: collaborative - boolean
+            3. BODY: description - String
 
         Tests
-            1. include name
-            2. exclude name
-            3. include name of empty String ""
-            4. no userId
-            5. invalid userId
-
+            Complete:
+            Incomplete:
+                Create_Playlist_01 - Payload has a valid name and user_id
+                Create_Playlist_02 - Provide an invalid user_id
+                Create_Playlist_03 - Provide an empty user_id
+                Create_Playlist_04 - Payload name is an empty String
+                Create_Playlist_05 - public as valid Boolean: true
+                Create_Playlist_06 - public as valid Boolean: false
+                Create_Playlist_07 - public as non-Boolean data type
+                Create_Playlist_08 - public as blank ""
+                Create_Playlist_09 - collaborative as valid Boolean: true
+                Create_Playlist_10 - collaborative as valid Boolean: false
+                Create_Playlist_11 - collaborative as non-Boolean data type
+                Create_Playlist_12 - collaborative as blank ""
+                Create_Playlist_13 - description as valid String
+                Create_Playlist_14 - description as non-String data type
+                Create_Playlist_15 - description as blank ""
      */
     @Test
     public void createPlaylist() {
