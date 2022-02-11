0bb6d0a90fd5be621263127decbe1a87cac44063
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-02-11 13:42:23 -0500

    added test from Playlists class

--- src/test/java/tests/Playlists/CHANGE_PLAYLIST_DETAILS.java
+++ src/test/java/tests/Playlists/CHANGE_PLAYLIST_DETAILS.java
@@ -1,4 +1,40 @@
 package tests.Playlists;
 
-public class CHANGE_PLAYLIST_DETAILS {
+import models.Playlist;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.restResources.RestfulPlaylist;
+
+public class CHANGE_PLAYLIST_DETAILS extends Playlists {
+    @Test
+    public void updatePlaylistDetails() {
+        // CREATE PLAYLIST OBJECT
+        // create request body
+        Playlist playlist = Playlist.generatePlaylist();
+
+        // CREATE PLAYLIST VIA API
+        Playlist  originalPlaylist = RestfulPlaylist.createPlaylist(userId, playlist);
+
+        // CREATE UPDATED PLAYLIST
+        Playlist playlist1 = new Playlist();
+        playlist1.setName("Updated Playlist " + System.currentTimeMillis());
+        playlist1.setDescription("This bad boiiiii has been updated");
+        playlist1.setPublic(true);
+
+        // SEND PUT REQUEST
+        RestfulPlaylist.updatePlaylistDetails(originalPlaylist.getId(), playlist1);
+
+        // GET PLAYLIST
+        Playlist resultingPlaylist = RestfulPlaylist.getPlaylist_byId(originalPlaylist.getId());
+
+        // ASSERT
+        // assert new & old data are different
+        Assert.assertNotEquals(resultingPlaylist.getName(), originalPlaylist.getName());
+        Assert.assertNotEquals(resultingPlaylist.getDescription(), originalPlaylist.getDescription());
+        Assert.assertNotEquals(resultingPlaylist.getPublic(), originalPlaylist.getPublic());
+        // assert the result has the updated values
+        Assert.assertEquals(resultingPlaylist.getName(), playlist1.getName());
+        Assert.assertEquals(resultingPlaylist.getDescription(), playlist1.getDescription());
+        Assert.assertEquals(resultingPlaylist.getPublic(), playlist1.getPublic());
+    }
 }
