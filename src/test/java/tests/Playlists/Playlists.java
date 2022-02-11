5a556ab0be67245b75678dc37965efc313481c9d
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-02-11 13:40:57 -0500

    added 'static'

--- src/test/java/tests/Playlists/Playlists.java
+++ src/test/java/tests/Playlists/Playlists.java
@@ -1,20 +1,11 @@
 package tests.Playlists;
-// JAVA
-import java.util.ArrayList;
-import java.util.List;
 // TEST-NG
-import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-// JSON
-import org.json.JSONObject;
 // REST-ASSURED
 import io.restassured.response.Response;
 // MINE
 import utils.Endpoints;
 import utils.restResources.RestResource;
-import utils.restResources.RestfulPlaylist;
-import models.Playlist;
 
 
 /**
@@ -22,60 +13,15 @@ import models.Playlist;
  */
 public class Playlists {
 
-    String userId;
+    static String userId;
 
     @BeforeTest
-    public void setup() {
+    public static void setup() {
         // GET USER's ID
         // send request
         Response userData = RestResource.get(Endpoints.ME);
         // extracting users' ID
         userId = userData.path("id");
-    }
-
-    //*************************
-    //*************************
-    //      DELETE TESTS
-    //*************************
-    //*************************
-
-
-    //*************************
-    //*************************
-    //       PUT TESTS
-    //*************************
-    //*************************
-
-    @Test
-    public void updatePlaylistDetails() {
-        // CREATE PLAYLIST OBJECT
-        // create request body
-        Playlist playlist = Playlist.generatePlaylist();
-
-        // CREATE PLAYLIST VIA API
-        Playlist  originalPlaylist = RestfulPlaylist.createPlaylist(userId, playlist);
-
-        // CREATE UPDATED PLAYLIST
-        Playlist playlist1 = new Playlist();
-        playlist1.setName("Updated Playlist " + System.currentTimeMillis());
-        playlist1.setDescription("This bad boiiiii has been updated");
-        playlist1.setPublic(true);
-
-        // SEND PUT REQUEST
-        RestfulPlaylist.updatePlaylistDetails(originalPlaylist.getId(), playlist1);
-
-        // GET PLAYLIST
-        Playlist resultingPlaylist = RestfulPlaylist.getPlaylist_byId(originalPlaylist.getId());
-
-        // ASSERT
-        // assert new & old data are different
-        Assert.assertNotEquals(resultingPlaylist.getName(), originalPlaylist.getName());
-        Assert.assertNotEquals(resultingPlaylist.getDescription(), originalPlaylist.getDescription());
-        Assert.assertNotEquals(resultingPlaylist.getPublic(), originalPlaylist.getPublic());
-        // assert the result has the updated values
-        Assert.assertEquals(resultingPlaylist.getName(), playlist1.getName());
-        Assert.assertEquals(resultingPlaylist.getDescription(), playlist1.getDescription());
-        Assert.assertEquals(resultingPlaylist.getPublic(), playlist1.getPublic());
     }
 }
 
